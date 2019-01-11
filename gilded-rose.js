a16be84b0b863c25254815bb1908f8a63c345945
Robin Pokorny <rpokorny@newstore.com>
2019-01-11 12:10:06 +0100

    :zap: Refactor with lodash/fp

--- gilded-rose.js
+++ gilded-rose.js
@@ -1,69 +1,51 @@
-/**
- * The update quality function
- * @example
- * updateQuality([
- *   { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
- *   { name: "Aged Brie", sellIn: 2, quality: 0 },
- *   { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
- *   { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
- *   {
- *     name: "Backstage passes to a TAFKAL80ETC concert",
- *     sellIn: 15,
- *     quality: 20
- *   },
- *   { name: "Conjured Mana Cake", sellIn: 3, quality: 6 }
- * ]);
- */
+const _ = require("lodash/fp");
 
-module.exports = function updateQuality(items) {
-  for (var i = 0; i < items.length; i++) {
-    if (
-      items[i].name != "Aged Brie" &&
-      items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-    ) {
-      if (items[i].quality > 0) {
-        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-          items[i].quality = items[i].quality - 1;
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1;
-        if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (items[i].sellIn < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-          if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-      items[i].sellIn = items[i].sellIn - 1;
-    }
-    if (items[i].sellIn < 0) {
-      if (items[i].name != "Aged Brie") {
-        if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (items[i].quality > 0) {
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-              items[i].quality = items[i].quality - 1;
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality;
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
-        }
-      }
-    }
-  }
+const moveSellIn = _.update("sellIn", _.add(-1));
 
-  return items;
-};
+const capQuality = _.update("quality", _.clamp(0, 50));
+
+const hasName = _.matchesProperty("name");
+
+const sellInRange = (start, end) =>
+  _.flow(
+    _.get("sellIn"),
+    _.inRange(start, end)
+  );
+
+const updateDefault = _.flow(
+  _.cond([
+    [sellInRange(-Infinity, 1), _.update("quality", _.add(-2))],
+    [_.stubTrue, _.update("quality", _.add(-1))]
+  ]),
+  capQuality,
+  moveSellIn
+);
+
+const updatedAged = _.flow(
+  _.cond([
+    [sellInRange(-Infinity, 1), _.update("quality", _.add(2))],
+    [_.stubTrue, _.update("quality", _.add(1))]
+  ]),
+  capQuality,
+  moveSellIn
+);
+
+const updatePasses = _.flow(
+  _.cond([
+    [sellInRange(6, 11), _.update("quality", _.add(2))],
+    [sellInRange(1, 6), _.update("quality", _.add(3))],
+    [sellInRange(-Infinity, 1), _.update("quality", _.constant(0))],
+    [_.stubTrue, _.update("quality", _.add(1))]
+  ]),
+  capQuality,
+  moveSellIn
+);
+
+const updateItemQuality = _.cond([
+  [hasName("Sulfuras, Hand of Ragnaros"), _.identity],
+  [hasName("Aged Brie"), updatedAged],
+  [hasName("Backstage passes to a TAFKAL80ETC concert"), updatePasses],
+  [_.stubTrue, updateDefault]
+]);
+
+module.exports = _.map(updateItemQuality);
